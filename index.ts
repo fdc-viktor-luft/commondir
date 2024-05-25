3735f38888691c370b7f0e2ded96752b3471e0f2
Viktor Luft <viktor.luft@freiheit.com>
2024-05-25 21:59:15 +0200

    Add some docs

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-const commonDirOfFiles = (absoluteFiles: string[]): string => {
+const commonDir = (absoluteFiles: string[]): string => {
     const res = absoluteFiles.slice(1).reduce((ps, file) => {
         const xs = file.split(/\/+|\\+/);
         let i = 0;
@@ -16,11 +16,13 @@ const commonDirOfFiles = (absoluteFiles: string[]): string => {
     return res.length > 1 ? res.join('/') : '/'
 }
 
-const commonDir = ((files: string[], {cwd, relative}: { cwd?: string; relative?: boolean } = {}): string => {
+/**
+ * commondir(["/x/y/z", "/x/y", "/x/y/w/q"]) === "/x/y"
+ */
+export default ((files: string[], { cwd, relative }: { cwd?: string; relative?: boolean } = {}): string => {
     const usedCwd = cwd || process.cwd();
     const absolutePaths = files.map((f) => path.isAbsolute(f) ? f : path.resolve(usedCwd, f));
-    const common = commonDirOfFiles(absolutePaths);
+    const common = commonDir(absolutePaths);
     return relative ? path.relative(usedCwd, common) : common;
 });
 
-export default commonDir;
